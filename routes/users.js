082cd3bf9bfedd55a1765c2b7a272deadd4064d7
DhulipudiAchyutSaiKrishna <63388971+DhulipudiAchyutSaiKrishna@users.noreply.github.com>
2023-02-19 23:58:43 +0530

    "Just added middlewares"

--- routes/users.js
+++ routes/users.js
@@ -71,18 +71,12 @@ router.post('/login', async (req, res)=>{
 });
 
 //to register a user using http post request
-router.post('/register', async (req, res)=>{
+router.post('/register', auth.requireEmployee, async (req, res)=>{
     let user = new User({
         name: req.body.name,
         email: req.body.email,
         passwordHash: req.body.passwordHash,
-        phone: req.body.phone,
-        isAdmin: req.body.isAdmin,
-        street: req.body.street,
-        apartment: req.body.apartment,
-        zip: req.body.zip,
-        city: req.body.city,
-        country: req.body.country,
+        role: req.body.role
     });
     user = await user.save();
 
@@ -93,7 +87,7 @@ router.post('/register', async (req, res)=>{
 });
 
 //To get the count of users
-router.get(`/get/count`, async (req, res)=>{
+router.get(`/get/count`, auth.requireAdmin, async (req, res)=>{
     const userCount = await User.countDocuments();
   
     if(!userCount){
@@ -105,8 +99,8 @@ router.get(`/get/count`, async (req, res)=>{
     });
   });
 
-//To delete a particular user by admin
-router.delete('/:id', (req, res) => {
+//To delete a particular user 
+router.delete('/:id', auth.requireEmployee, (req, res) => {
     User.findByIdAndRemove(req.params.id).then(user => {
         if (user){
             return res.status(200).json({success: true, message: 'The user is deleted'})
